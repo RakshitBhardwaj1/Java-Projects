8af2d18b7201258f924724fc896e118d7ce91801
LENOVO <LENOVO@LAPTOP-MBAKBEIB.bbrouter>
2024-08-20 23:55:06 +0530

    This is a First oops lab

---
+++ java_lab4_oops_2/src/java_lab4_oops_2/Area.java
@@ -0,0 +1,16 @@
+package java_lab4_oops_2;
+
+public class Area {
+	int Length;
+	int Breath;
+	public void area_rect() {
+		System.out.println("Area of a Rectangle is "+Length*Breath);
+	}
+	public void peri_rect() {
+		System.out.println("Area of a Rectangle is "+2*(Length+Breath));	
+	}
+}
+
+WAP TO CALCULATE TOTAL SURFACE AREA AND VOLUME OF CYLINDER
+WAP TO CALCULATE SIMPLE AND COMPOUND INTEREST
+
---
+++ java_lab4_oops_2/src/java_lab4_oops_2/Rect_attribute.java
@@ -0,0 +1,16 @@
+package java_lab4_oops_2;
+
+public class Rect_attribute {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Area st1=new Area();
+		st1.Length=56;
+		st1.Breath=60;
+		
+		st1.area_rect();
+		st1.peri_rect();
+
+	}
+
+}
